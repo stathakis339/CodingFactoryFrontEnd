98215314a97b3f016c31250413dd90f61b68dc81
Lefteris Stathakis <stathakislefteris@outlook.com>
2024-04-09 23:23:46 +0300

    Create elements , choose, style with js

--- chapter14/js/appened.js
+++ chapter14/js/appened.js
@@ -1,6 +1,12 @@
-const container = document.querySelector('.container')  //relation
-// container.innerHTML += '<p> Hello CF!!! </p>'            //Add paragraph inside .container
+const container = document.querySelector('#container')  //relation
+container.classList.add('container')                    //Add class
+container.innerHTML += '<p id="hello"> Hello CF!!! </p>'            //Add paragraph inside .container
+const hello = document.querySelector('#hello')
+const cf = document.querySelector('#cf')
 
-const p = document.createElement('p')
-p.textContent= 'Hello World!!!'                         //We create subnode
-container.appendChild(p)                                //Appened in parrent
+hello.style.textAlign = 'center'
+cf.style.textAlign = 'center'
+
+// const p = document.createElement('p')
+// p.textContent= 'Hello World!!!'                         //We create subnode
+// container.appendChild(p)                                //Appened in parrent
