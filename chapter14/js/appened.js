bc9be35a3eb6454f56825dfb6364879fb2435245
Lefteris Stathakis <stathakislefteris@outlook.com>
2024-04-11 20:06:13 +0300

    We use cloneNode, dynamic style, event handle

--- chapter14/js/appened.js
+++ chapter14/js/appened.js
@@ -1,11 +1,20 @@
-const container = document.querySelector('#container')  //relation
-container.classList.add('container')                    //Add class
-container.innerHTML += '<p id="hello"> Hello CF!!! </p>'            //Add paragraph inside .container
+const container = document.querySelector('#container')     //relation
+// container.classList.add('container')                    //Add class
+// container.classList.add('container','box-center')       
+const cls = ['container','box-center']          
+container.classList.add(...cls)                            //Destructure the elements of array
+
+container.innerHTML += '<p id="hello"> Hello CF!!! </p>'   //Add paragraph inside .container
 const hello = document.querySelector('#hello')
 const cf = document.querySelector('#cf')
 
-hello.style.textAlign = 'center'
-cf.style.textAlign = 'center'
+// hello.style.textAlign = 'center'
+// cf.style.textAlign = 'center'
+
+
+
+hello.classList.add('center')
+cf.classList.add('center')
 
 // const p = document.createElement('p')
 // p.textContent= 'Hello World!!!'                         //We create subnode
