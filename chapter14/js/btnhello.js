1ba09cc76a087b6f4f417202b3727d8048681a15
Lefteris Stathakis <stathakislefteris@outlook.com>
2024-04-12 18:36:13 +0300

    Update btnhello

--- chapter14/js/btnhello.js
+++ chapter14/js/btnhello.js
@@ -1,3 +1,12 @@
-function sayHello() {
+const helloBtn = document.querySelector('#helloBtn')    //QuerySelctor return is a reference to a node(button is a node),every node has properties one of them is onClick.
+helloBtn.addEventListener('click', function() {         //With addEventListener we can define many handlers at the same or deferents events at the same html element and we can set the execution priotity of handlers when we have nested elements 
     alert('Hello Conding Factory')
-}
+})
+
+// helloBtn.onclick = function() {
+//     alert('Hello Conding Factory')
+// }
+
+// function sayHello() {
+//     alert('Hello Conding Factory')
+// }
